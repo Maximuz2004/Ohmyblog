81171f012561ab3852d2b9ac2aa5c467f026d4d4
Maximuz2004 <matitov2004@gmail.com>
2023-07-13 17:51:23 +0300

    post model admin register and more

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'slug',
+        'author',
+        'body',
+        'publish',
+        'status',
+    )
+    search_fields = ('title', 'body',)
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    list_filter = ('author', 'status', 'created', 'publish')
+    empty_value_display = '--пусто--'
