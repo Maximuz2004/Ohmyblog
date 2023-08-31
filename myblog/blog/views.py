b5ca636f65249f7c7754c49627662b3f9abf56b1
Максим Титов <matitov2004@gmail.com>
2023-09-01 01:31:33 +0300

    search form

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.postgres.search import SearchVector
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
@@ -8,7 +9,7 @@ from django.shortcuts import get_object_or_404, render
 
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Comment, Post
 
 
@@ -102,3 +103,25 @@ def post_comment(request, post_id):
                        'form': form,
                        'comment': comment}
                       )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
+        }
+    )
